4e50ec8fde89f1c9d065cb9ed244f83dadfc82c7
John T. Hwang <jhwang@eng.ucsd.edu>
2022-01-07 18:23:06 -0800

    changed generated bib files to html files

--- write_individual_bib_files.py
+++ write_individual_bib_files.py
@@ -5,11 +5,29 @@ import pybtex
 import bibtexparser
 
 
+bibtex_string = \
+r"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Bibtex for {}
+</title>
+</head>
+<body>
+<pre>
+{}
+</pre>
+</body>
+</html>
+"""
+
 bib_data_dict = load_bib_data_dict(lsdo_conference=True, lsdo_journal=True)
 
 for key, ref in iteritems(bib_data_dict):
     bib_database_ = pybtex.database.BibliographyData({key: ref})
     bib_string = bib_database_.to_string('bibtex')
+    html_string = bibtex_string.format(key, bib_string[:-1])
 
-    with open('generated_bib_files/{}.bib'.format(key), 'w') as bibtex_file:
-        bibtex_file.write(bib_string[:-1])
+    with open('generated_bib_files/{}.html'.format(key), 'w') as bibtex_file:
+        bibtex_file.write(html_string)
