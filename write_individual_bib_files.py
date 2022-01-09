00ac17c4c1a4a9ccf5fa4ea2f81824584e58de3d
John T. Hwang <jhwang@eng.ucsd.edu>
2022-01-08 23:25:38 -0800

    added pdf directory and renamed .html files to .bib files; added .gitignore

--- write_individual_bib_files.py
+++ write_individual_bib_files.py
@@ -27,7 +27,6 @@ bib_data_dict = load_bib_data_dict(lsdo_conference=True, lsdo_journal=True)
 for key, ref in iteritems(bib_data_dict):
     bib_database_ = pybtex.database.BibliographyData({key: ref})
     bib_string = bib_database_.to_string('bibtex')
-    html_string = bibtex_string.format(key, bib_string[:-1])
 
-    with open('bib/{}.html'.format(key), 'w') as bibtex_file:
-        bibtex_file.write(html_string)
+    with open('bib/{}.bib'.format(key), 'w') as bibtex_file:
+        bibtex_file.write(bib_string[:-1])
