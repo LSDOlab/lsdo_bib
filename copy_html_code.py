5e10a2d32a1ef9c60361fe5ce11dd997a4e90ce8
John T. Hwang <jhwang@eng.ucsd.edu>
2022-03-05 10:02:02 -0800

    html code generator set up to work for ULI website also now

--- copy_html_code.py
+++ copy_html_code.py
@@ -2,13 +2,36 @@ from utils.html_writer import HTMLWriter
 from utils.load_bib_data import load_bib_data_dict
 
 
+# -------------------- Options
+
 # mode = 'by journal/conference'
 mode = 'by year'
 
+website = 'lsdo'
+# website = 'uli'
+
+# -------------------- Keys
 
 lsdo_journal_keys = list(load_bib_data_dict(lsdo_journal=True).keys())
 lsdo_conference_keys = list(load_bib_data_dict(lsdo_conference=True).keys())
 lsdo_keys = lsdo_journal_keys + lsdo_conference_keys
+uli_journal_keys = [
+    'zhao2022open',
+]
+uli_conference_keys = [
+]
+uli_keys = uli_journal_keys + uli_conference_keys
+
+if website is 'lsdo':
+    journal_keys = lsdo_journal_keys
+    conference_keys = lsdo_conference_keys
+    keys = lsdo_keys
+elif website is 'uli':
+    journal_keys = uli_journal_keys
+    conference_keys = uli_conference_keys
+    keys = uli_keys
+
+# -------------------- html_writer
 
 full_bib_data_dict = load_bib_data_dict(lsdo_journal=True, lsdo_conference=True, external=True)
 
@@ -16,9 +39,9 @@ html_writer = HTMLWriter()
 html_writer.write_header()
 
 if mode is 'by journal/conference':
-    html_writer.write_lines_by_type(full_bib_data_dict, lsdo_journal_keys, 'journal')
-    html_writer.write_lines_by_type(full_bib_data_dict, lsdo_conference_keys, 'conference')
+    html_writer.write_lines_by_type(full_bib_data_dict, journal_keys, 'journal')
+    html_writer.write_lines_by_type(full_bib_data_dict, conference_keys, 'conference')
 elif mode is 'by year':
-    html_writer.write_lines_by_year(full_bib_data_dict, lsdo_keys)
+    html_writer.write_lines_by_year(full_bib_data_dict, keys)
 
 html_writer.copy2clipboard()
